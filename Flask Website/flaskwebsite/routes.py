5d655b7d9cc28f148d5db071a573616dff6a2659
Pranav-19 <pranavahuja1@JAGDISHs-MacBook-Air.local>
2019-07-10 13:51:32 +0530

    routes commit 1

--- Flask Website/flaskwebsite/routes.py
+++ Flask Website/flaskwebsite/routes.py
@@ -1,4 +1,19 @@
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskwebsite import app, db, bcrypt
-from flaskwebsite.forms import PostForm
+from flaskwebsite.forms import ReviewForm
 from flaskwebsite.models import Post
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+@app.route('/books')
+def bookpage():
+    return render_template("books.html")
+@app.route('/movies')
+def moviepage():
+    return render_template("movies.html")
+@app.route('/faq')
+def faqpage():
+    return render_template("faq.html")
+@app.route('/reviews')
+def reviewpage():
+    return render_template("reviews.html")
