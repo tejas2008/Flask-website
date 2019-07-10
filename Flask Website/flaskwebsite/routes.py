5d0b5ea32a32db18500e724b4c02b9bfaea36567
tejas2008 <tejas.khanolkar@somaiya.edu>
2019-07-10 19:46:39 +0530

    header and bakground added

--- Flask Website/flaskwebsite/routes.py
+++ Flask Website/flaskwebsite/routes.py
@@ -1,18 +1,22 @@
-from flask import render_template, url_for, flash, redirect, request, abort
+from flask import Flask render_template, url_for, flash, redirect, request, abort
 from flaskwebsite import app, db
 from flaskwebsite.forms import PostForm
 from flaskwebsite import app, db, bcrypt
 from flaskwebsite.forms import ReviewForm
 from flaskwebsite.models import Post
+app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template("index.html")
 @app.route('/books')
 def bookpage():
     return render_template("books.html")
-@app.route('/movies')
-def moviepage():
-    return render_template("movies.html")
+@app.route('/movies1')
+def moviepage1():
+    return render_template("movies1.html")
+@app.route('/movies2')
+def moviepage2():
+    return render_template("movies2.html")
 @app.route('/faq')
 def faqpage():
     return render_template("faq.html")
@@ -22,3 +26,5 @@ def faqpage():
 def reviewpage():
     return render_template("reviews.html")
 
+if __name__ == '__main__':
+    app.run(debug=True)
