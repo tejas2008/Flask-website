d830a06ab070c7ab7304fdd6560f9563748c48f3
omrawal <om.rawal@somaiya.edu>
2019-07-10 13:58:25 +0530

    om

--- Flask Website/flaskwebsite/routes.py
+++ Flask Website/flaskwebsite/routes.py
@@ -1,5 +1,18 @@
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskwebsite import app, db, bcrypt
+from flaskwebsite import app, db
 from flaskwebsite.forms import PostForm
 from flaskwebsite.models import Post
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+@app.route('/books')
+def bookpage():
+    return render_template("books.html")
+@app.route('/movies')
+def moviepage():
+    return render_template("movies.html")
+@app.route('/faq')
+def faqpage():
+    return render_template("faq.html")
+
 
